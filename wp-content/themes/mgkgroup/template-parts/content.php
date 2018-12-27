459fa2091101293f2fc5731ea4d11ffd11eee64a
hatazu <hatazu@gmail.com>
2018-12-27 11:46:18 +0700

    thumbnail

--- wp-content/themes/mgkgroup/template-parts/content.php
+++ wp-content/themes/mgkgroup/template-parts/content.php
@@ -16,7 +16,16 @@ if ( is_single() ) :
 <div class="single_post_text">
 	<?php the_content(); ?>
 </div>
-<?php else : ?>
+<?php else : 
+	$id_post = get_the_ID();
+	  $thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id($id_post), 'medium', false );
+	   if(!$thumbnail){
+	     //$rand = rand(0,4);
+	    //$$_thumbnail = no_thumbnail_url."no-thumbnail".$rand.".jpg";
+	  }else{
+	    $_thumbnail = $thumbnail[0];
+	  }
+?>
   <!-- Blog post -->
   <div class="blog_post">
     <div class="blog_image" style="background-image:url(<?php echo $_thumbnail; ?>)"></div>
